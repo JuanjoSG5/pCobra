a4187df16f62311f312045ffce4e7cacfa3ad8d8
JuanjoSG5 <113671548+JuanjoSG5@users.noreply.github.com>
2025-07-19 07:53:00 +0200

    fix: fixed an issue with the  pathing

--- examples/tutorial_basico/hola_mundo.py
+++ examples/tutorial_basico/hola_mundo.py
@@ -1,5 +1,14 @@
+import sys
+import os
+
+parent = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+
+# Add the sibling project folder
+sys.path.insert(0, parent)
+backend_path = os.path.join(parent, 'backend')
+sys.path.insert(0, os.path.join(backend_path, 'src'))
+sys.path.insert(0, os.path.join(backend_path, 'corelibs'))
+
 from core.nativos import *
-from corelibs import *
 from standard_library import *
 print('Hola, mundo!')
-
